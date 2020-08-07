ba24c04c338a421633dafee4d38193573ff8c1e6
Megan Hepner <megan.hepner@gmail.com>
2020-08-07 13:58:07 -0700

    connect viewbag to homecontroller

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
 
-    [HttpGet("/")]
+    // [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.MachineId.ToList();
+      ViewBag.EngineerId.ToList();
       return View();
     }
 
